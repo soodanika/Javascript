f7d15ad205464ef8ea3c99d33a695bd8c5372037
sood <soodanika2000@gmail.com>
2024-02-09 19:20:07 +0530

    Memoize concept

---
+++ Leetcode/Memoize.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn, cache = {}) {
+    return function (...args) {
+        var key = String(args)
+        if (cache.hasOwnProperty(key)) return cache[key]
+        return cache[key] = fn(...args);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
