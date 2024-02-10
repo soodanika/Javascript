3de0293167173598f097ea6759bde0aa666bafaa
sood <soodanika2000@gmail.com>
2024-02-10 11:41:35 +0530

    SetTimeout related problem

---
+++ Leetcode/Timeout Cancellation.js
@@ -0,0 +1,38 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+const cancellable = function (fn, args, t) {
+    var timeout = setTimeout(() => {
+        fn(...args)
+    }, t)
+    var cancelFn = function () {
+        clearTimeout(timeout);
+    }
+
+    // If we call the function, it will return cancelFn,
+    // and since the return line calls (and consequentially executes)
+    // cancelFn, timeout will be cancelled, thereby cancelling fn
+    return cancelFn;
+};
+
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(fn, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
